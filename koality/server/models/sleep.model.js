b87f8eab9bec8e2b11c39625e22d75c09fba308f
JilianaTiu <jilianatiu@gmail.com>
2021-12-14 19:30:20 -0800

    added Sleep model

---
+++ koality/server/models/sleep.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const Sleep = mongoose.model(
+    "Sleep",
+    new mongoose.Schema({
+        sleepGoal: Number,
+        sleepHours: Number,
+        sleepBedtime: Date,
+    })
+);
+
+module.exports = User;
