d2bf8e77a84d627d054bb4698db69cae5a45d61f
fionachn <66981052+chenfi-afk@users.noreply.github.com>
2022-01-04 19:25:43 -0800

    food and home page

--- koality/pages/HomeScreen.js
+++ koality/pages/HomeScreen.js
@@ -4,16 +4,71 @@ import {
   StyleSheet,
   View,
   Text,
-  SafeAreaView
+  SafeAreaView,
+  Button,
+  Alert,
 } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
+
+  const username = "<user>";
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
-        
+      <View>
+        <Text style={styles.titleText}>
+        Welcome, {username}!
+        </Text>
+
+        <Text style={styles.title}>
+        How are you feeling today?
+        </Text>
+        <View style={styles.fixToText}>
+          <Button
+            title="Happy"
+            onPress={() => Alert.alert('Happy button pressed')}
+          />
+          <Button
+            title="Calm"
+            onPress={() => Alert.alert('Calm button pressed')}
+          />
+          <Button
+            title="Relaxed"
+            onPress={() => Alert.alert('Relaxed button pressed')}
+          />
+          <Button
+            title="Focused"
+            onPress={() => Alert.alert('Focused button pressed')}
+          />
+          <Button
+            title="Sad"
+            onPress={() => Alert.alert('Sad button pressed')}
+          />
+          <Button
+            title="Agitated"
+            onPress={() => Alert.alert('Agitated button pressed')}
+          />
+          <Button
+            title="Tired"
+            onPress={() => Alert.alert('Tired button pressed')}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
 export default HomeScreen;
